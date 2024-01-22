317b8f17beea28946b785a9f182ce8063f083bfb
vova11 <vladimir.zembera@gmail.com>
2024-01-22 11:21:39 +0100

    nicer form in product

--- src/components/products/CreateProduct.tsx
+++ src/components/products/CreateProduct.tsx
@@ -1,55 +1,43 @@
-import { type FC } from 'react'
-import { useRef, type FormEvent } from 'react'
+import { type FC, useRef } from 'react'
 import { type Product } from './Products'
+import { Button, Input, Form, FormHandle } from '../utils'
+
 type createProductProps = {
   onAddProduct: (product: Product) => void
 }
 
 export const CreateProduct: FC<createProductProps> = ({ onAddProduct }) => {
-  const formRefs = {
-    title: useRef<HTMLInputElement>(null),
-    description: useRef<HTMLInputElement>(null),
-    quantity: useRef<HTMLInputElement>(null),
-  }
+  const customForm = useRef<FormHandle>(null)
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-
-    const product = {
-      id: Math.floor(Math.random() * 100) + 1,
-      title: formRefs.title.current!.value,
-      description: formRefs.description.current!.value,
-      quantity: parseInt(formRefs.quantity.current!.value, 10),
+  const handleSave = (data: unknown) => {
+    const extractedData = data as {
+      id: string
+      title: string
+      description: string
+      quantity: number
     }
 
-    event.currentTarget.reset()
+    const product: Product = {
+      id: String(Math.floor(Math.random() * 100) + 1), // Convert the number to a string
+      title: extractedData.title,
+      description: extractedData.description,
+      quantity: extractedData.quantity,
+    }
+
+    customForm.current?.clear()
     // Call onAddProduct with the product object
     onAddProduct(product)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSave={handleSave} ref={customForm}>
+      <Input id='title' label='Product title' type='text' />
+      <Input id='description' label='Product description' type='text' />
+      <Input id='quantity' label='Product quantity' type='number' />
       <p>
-        <label htmlFor='product'>Product title</label>
-        <input type='text' id='product' ref={formRefs.title} />
+        <Button>Create Product</Button>
       </p>
-      <p>
-        <label htmlFor='product'>Product description</label>
-        <input
-          type='text'
-          name=''
-          id='description'
-          ref={formRefs.description}
-        />
-      </p>
-      <p>
-        <label htmlFor='product'>Product quantity</label>
-        <input type='number' id='quantity' ref={formRefs.quantity} />
-      </p>
-      <p>
-        <button type='submit'>Add product</button>
-      </p>
-    </form>
+    </Form>
   )
 }
 
